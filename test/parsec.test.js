c96fcefd911802c5a3375dd3007e76af1bd3e304
ttch <ttchgm@gmail.com>
2016-01-31 22:11:57 +0800

    add bind over then by parsec test

--- test/parsec.test.js
+++ test/parsec.test.js
@@ -12,32 +12,27 @@ describe('parsec',function (){
     describe('eq',function () {
         it("eq test",function(){
         	
-            var st = psc.state('a');
-            var p = psc.atom().eq('a');
-            expect(p(st)).to.be.equal('a');
+            var expr = (st = 
+                () => psc.state('a')) =>
+                    psc.atom().eq('a') (st());
+            
+            expect(expr()).to.be.equal('a');
         });
         it('one',function(){
             var expr = (st = 
                 () => psc.state('abc')) =>
-                        psc.atom().one('a')(st()
-            );
+                        psc.atom().one('a')(st());
 
             expect(expr()).to.be.equal('a');
         });
         it('one of',function(){
-            var state = psc.state('abc');
-            var oneOf = psc.atom().oneOf('q','w','e','r','t','a');
-            
-            var re = oneOf(state);
-            expect(re).to.be.equal('a');
+            var expr = ( st = () => psc.state('abc') ) =>
+                psc.parsec( psc.atom().oneOf('a','w','e','r','t','b') ) ( st() )
+
+
+            expect(expr()).to.be.equal('a');
         });
         it('none of',function(){
-            /*
-            var state = psc.state('abc');
-            var noneOf = psc.atom().noneOf('q','w','e','r','t','b');
-            var re = noneOf(state);
-            */
-
             var expr = ( st = () => psc.state('abc') ) =>
                 psc.parsec( psc.atom().noneOf('q','w','e','r','t','b')) ( st() )
 
@@ -51,13 +46,30 @@ describe('parsec',function (){
             var fail = psc.atom().fail();
             //todo
         });
-        it('parsec',function(){
+        it('parsec then',function(){
             var expr = (st = 
                 () => psc.state('abc')) =>
                         psc.parsec( psc.atom().one('c') )
                             .then( psc.atom().one('b') ) (st());
 
             expect(expr()).to.be.equal('b');
+        });
+
+        it('parsec bind',function(){
+            var expr = (st = 
+                () => psc.state('abc')) =>
+                        psc.parsec( psc.atom().one('c') )
+                            .bind( (x,state) =>
+                             "b" ) (st());
+            expect(expr()).to.be.equal('b');
+        });
+        it('parsec over',function(){
+            var expr = (st = 
+                () => psc.state('abc')) =>
+                        psc.parsec( psc.atom().one('a') )
+                            .over( psc.atom().one('b') ) (st());
+
+            expect(expr()).to.be.equal('a');
         });
 
     });
